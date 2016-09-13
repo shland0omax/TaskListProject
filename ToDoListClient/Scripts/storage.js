87d6e4fa6b05e274c77ef4457b2dbe5af94cd984
Максим Михайлович Шландаков <shland0omax@gmail.com>
2016-09-13 14:07:24 +0300

    local interaction added. Pending for server connect

---
+++ ToDoListClient/Scripts/storage.js
@@ -0,0 +1,48 @@
+﻿var index = 0;
+
+function savetolocal() {
+    var model = {};
+    model.Name = $("#newName").val();
+    model.IsCompleted = $("#newCompleted").is(":checked");
+    model.IsDirty = true;
+    model.ToDoId = "l" + (index + 1);
+    localStorage.setItem("l" + (index++),
+        JSON.stringify(model));
+    console.log("Logged.");
+    return model;
+}
+
+var displayTasks = function(parentSelector, tasks) {
+    $(parentSelector).empty();
+    $.each(tasks, function(i, item) {
+        appendRow(parentSelector, item);
+    });
+}
+
+var appendRow = function (parentSelector, obj) {
+    var tr = $("<tr data-id='" + obj.ToDoId + "'></tr>");
+    tr.append("<td><input type='checkbox' class='completed' " + (obj.IsCompleted ? "checked" : "") + "/></td>");
+    tr.append("<td class='name' >" + obj.Name + "</td>");
+    tr.append("<td><input type='button' class='delete-button' value='Delete' /></td>");
+    $(parentSelector).append(tr);
+}
+
+$(function () {
+    var tasks = [];
+    for (var item in localStorage) {
+        tasks.push(JSON.parse(localStorage.getItem(item)));
+    }
+    displayTasks("#tasks > tbody", tasks);
+
+    $("#newCreate").click(function(parent, tasks) {
+        var obj = savetolocal();
+        appendRow("#tasks > tbody", obj);
+    });
+
+    $('#tasks > tbody').on('click', '.delete-button', function (event) {
+        var task = $(this).parent().parent();
+        localStorage.removeItem(task.attr("data-id"));
+        task.remove();
+    });
+});
+
